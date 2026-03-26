fdf4c413cee8c447ead83880e038fc3cc81b89c3
RiyaSingh-77 <151558616+RiyaSingh-77@users.noreply.github.com>
2026-03-26 15:33:35 +0530

    fix: make food partner profile route public, fix CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-//create server
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
@@ -6,30 +5,21 @@ const foodRoutes = require('./routes/food.routes');
 const foodPartnerRoutes = require('./routes/food-partner.routes');
 const cors = require('cors');
 
-
-
-//Creation of a server:
-
 const app = express();
-app.use(cookieParser()); // USED AS A MIDDLEWARE
-app.use(express.json());
+
 app.use(cors({
-    origin: "https://munchly-dev-feed.vercel.app",
+    origin: ["https://munchly-dev-feed.vercel.app", "http://localhost:5173"],
     credentials: true
 }));
+app.use(cookieParser());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World");
-})
+});
 
-
-app.use('/api/auth', authRoutes); //To access the APIs of auth, we need to add a prefix
-//Here we are telling our server that there exists some APIs related to auth
+app.use('/api/auth', authRoutes);
 app.use('/api/food', foodRoutes);
 app.use('/api/food-partner', foodPartnerRoutes);
-
-
-
-
 
 module.exports = app;
--- src/controllers/food-partner.controller.js
+++ src/controllers/food-partner.controller.js
@@ -3,28 +3,42 @@ const foodPartnerModel = require('../models/foodpartner.model');
 
 
 async function getFoodPartnerById(req, res){
+    try {
+        const foodPartnerId = req.params.id;
 
+        const foodPartner = await foodPartnerModel.findById(foodPartnerId)
+        const foodItemsByFoodPartner = await foodModel.find({ foodPartner: 
+            foodPartnerId })
 
-    const foodPartnerId = req.params.id;
-
-    const foodPartner = await foodPartnerModel.findById(foodPartnerId)
-    const foodItemsByFoodPartner = await foodModel.find({ foodPartner: 
-        foodPartnerId })
-
-    if(!foodPartner){
-        return res.status(404).json({ message: "Food partner not found" });
-    }
-    res.status(200).json({
-        message: "Food partner retrieved successfully",
-        foodPartner: {
-            ...foodPartner.toObject(),
-            foodItems: foodItemsByFoodPartner
+        if(!foodPartner){
+            return res.status(404).json({ message: "Food partner not found" });
         }
-    });
-
-
+        res.status(200).json({
+            message: "Food partner retrieved successfully",
+            foodPartner: {
+                ...foodPartner.toObject(),
+                foodItems: foodItemsByFoodPartner
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
 }
 
+async function getMe(req, res) {
+    try {
+        const foodPartner = await foodPartnerModel.findById(req.foodPartner._id);
+        if (!foodPartner) {
+            return res.status(404).json({ message: "Food partner not found" });
+        }
+        res.status(200).json({ foodPartner });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}
+
+
 module.exports = {
-    getFoodPartnerById
+    getFoodPartnerById,
+    getMe
 };
